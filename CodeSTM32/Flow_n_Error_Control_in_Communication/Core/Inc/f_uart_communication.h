2269952fe533a93674fb4690809deaea11557aa8
Vy Nguyen <tuyetvynguyen18@gmail.com>
2023-12-14 17:33:57 +0700

    Add uart communication

--- CodeSTM32/Flow_n_Error_Control_in_Communication/Core/Inc/f_uart_communication.h
+++ CodeSTM32/Flow_n_Error_Control_in_Communication/Core/Inc/f_uart_communication.h
@@ -8,6 +8,14 @@
 #ifndef INC_F_UART_COMMUNICATION_H_
 #define INC_F_UART_COMMUNICATION_H_
 
+#include "main.h"
+#include "f_timer.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "f_global.h"
+#include "f_command_parser.h"
 
+void uart_communication_fsm(ADC_HandleTypeDef hadc1,
+					UART_HandleTypeDef huart2);
 
 #endif /* INC_F_UART_COMMUNICATION_H_ */
