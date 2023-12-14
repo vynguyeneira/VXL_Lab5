f16a7589b43832c2fd79f8ccf3bea5466bc57202
Vy Nguyen <tuyetvynguyen18@gmail.com>
2023-12-14 17:51:33 +0700

    Add command_parser

--- CodeSTM32/Flow_n_Error_Control_in_Communication/Core/Inc/f_command_parser.h
+++ CodeSTM32/Flow_n_Error_Control_in_Communication/Core/Inc/f_command_parser.h
@@ -8,6 +8,14 @@
 #ifndef INC_F_COMMAND_PARSER_H_
 #define INC_F_COMMAND_PARSER_H_
 
+#include "main.h"
+#include "string.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "math.h"
+#include "f_global.h"
+#include "f_timer.h"
 
+void command_parser_fsm();
 
 #endif /* INC_F_COMMAND_PARSER_H_ */
--- CodeSTM32/Flow_n_Error_Control_in_Communication/Core/Src/f_command_parser.c
+++ CodeSTM32/Flow_n_Error_Control_in_Communication/Core/Src/f_command_parser.c
@@ -7,4 +7,44 @@
 
 #include "f_command_parser.h"
 
+void command_parser_fsm()
+{
+	switch (status_parser)
+	{
+		case INIT_STR:
+			//If string starts with character '!', status = WAIT_END
+			//begins reading the command
+			if(temp == '!')
+			{
+				status_parser = WAIT_END;
+				command_index = 0;
+			}
+			break;
 
+		case WAIT_END:
+			//If string ends with character '#', status = INIT_STR, save the command to
+			// analysis in uart_communication_fsm function, flag = 1
+			if (temp == '#')
+			{
+				status_parser = INIT_STR;
+				command[command_index] = '\0';
+				command_flag = 1;
+			}
+
+			else
+			{
+			//If receive char '!', reset command_index, read again the command
+				if (temp == '!')
+					command_index = 0;
+				else
+				{
+					command[command_index++] = temp;
+					if (command_index == MAX_BUFFER_SIZE) command_index = 0;
+				}
+			}
+			break;
+
+		default:
+			break;
+	}
+}
